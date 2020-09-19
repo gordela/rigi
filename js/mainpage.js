7b2ed3db1df7e8abba2e8da49ed30d85f78587d4
gordela <giorgi.gordiashvili.1@btu.edu.ge>
2020-09-20 01:26:01 +0400

    firefox fix

--- js/mainpage.js
+++ js/mainpage.js
@@ -4,94 +4,208 @@ var timer;
 var loading = 0;
 var loadingTimer;
 
-var load = setInterval(function () {
-  $(".loading").text(loading + "%");
-  loading++;
-  let gray = "grayscale(" + (100 - loading) + "%)";
+if (checkBrowser() == "Firefox") {
+  var load = setInterval(function () {
+    $(".loading").text(loading + "%");
+    loading++;
+    let gray = "grayscale(" + (100 - loading) + "%)";
 
-  $(".logo-loading").css("filter", gray);
+    $(".logo-loading").css("filter", gray);
 
-  if (loading == 101) {
-    clearInterval(load);
-    $(".loading-logo").fadeOut();
-    $(".anim-image").attr("src", "./mainpage/1.png");
-    $(window).bind("mousewheel", function (event) {
-      if (timer) {
-        window.clearTimeout(timer);
-      }
+    if (loading == 101) {
+      clearInterval(load);
+      $(".loading-logo").fadeOut();
+      $(".anim-image").attr("src", "./mainpage/1.png");
+      $(window).bind("DOMMouseScroll", function (event) {
+        if (timer) {
+          window.clearTimeout(timer);
+        }
 
-      timer = window.setTimeout(function () {
-        if (oddeven % 2 == 0) {
-          if (event.originalEvent.wheelDelta >= 0) {
-            ++oddeven;
-            if (counter > 1) {
-              $(".anim-image").fadeOut();
-              $(".anim-image-2").fadeIn();
-              $(".anim-image-2").attr(
-                "src",
-                "./mainpage/" + --counter + ".png"
-              );
-              $(".red").css("width", String(counter / 37) * 100 + "%");
-            }
-          } else {
-            --oddeven;
-            if (counter < 38) {
-              if (counter != 37) {
-                $(".anim-image").fadeOut();
-                $(".anim-image-2").fadeIn();
+        timer = window.setTimeout(function () {
+          if (oddeven % 2 == 0) {
+            if (event.originalEvent.detail < 0) {
+              ++oddeven;
+              if (counter > 1) {
                 $(".anim-image-2").attr(
                   "src",
-                  "./mainpage/" + ++counter + ".png"
+                  "./mainpage/" + --counter + ".png"
                 );
+                $(".anim-image").fadeOut(function () {
+                  $(".anim-image-2").fadeIn();
+                });
 
-                switchCases(counter);
+                $(".red").css("width", String(counter / 37) * 100 + "%");
+              }
+            } else {
+              --oddeven;
+              if (counter < 38) {
+                if (counter != 37) {
+                  $(".anim-image-2").attr(
+                    "src",
+                    "./mainpage/" + ++counter + ".png"
+                  );
+                  $(".anim-image").fadeOut(function () {
+                    $(".anim-image-2").fadeIn();
+                  });
 
-                $(".red").css("width", String((counter / 37) * 100) + "%");
-              } else {
-                $(".anim-image-2").attr(
-                  "src",
-                  "./mainpage/" + counter + ".png"
-                );
-                $(".red").css("width", String((counter / 37) * 100) + "%");
+                  switchCases(counter);
+
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                } else {
+                  $(".anim-image-2").attr(
+                    "src",
+                    "./mainpage/" + counter + ".png"
+                  );
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                }
               }
             }
-          }
-        } else {
-          if (event.originalEvent.wheelDelta >= 0) {
-            ++oddeven;
-            if (counter > 1) {
-              $(".anim-image-2").fadeOut();
-              $(".anim-image").fadeIn();
-              $(".anim-image").attr("src", "./mainpage/" + --counter + ".png");
-              switchCases(counter);
-              $(".red").css("width", String(counter / 37) * 100 + "%");
-            }
           } else {
-            --oddeven;
-            if (counter < 38) {
-              if (counter != 37) {
-                $(".anim-image-2").fadeOut();
-                $(".anim-image").fadeIn();
+            if (event.originalEvent.detail < 0) {
+              ++oddeven;
+              if (counter > 1) {
                 $(".anim-image").attr(
                   "src",
-                  "./mainpage/" + ++counter + ".png"
+                  "./mainpage/" + --counter + ".png"
                 );
-                switchCases(counter);
+                $(".anim-image-2").fadeOut(function () {
+                  $(".anim-image").fadeIn();
+                });
 
-                $(".red").css("width", String((counter / 37) * 100) + "%");
-              } else {
-                $(".anim-image").attr("src", "./mainpage/" + counter + ".png");
-                $(".red").css("width", String((counter / 37) * 100) + "%");
+                switchCases(counter);
+                $(".red").css("width", String(counter / 37) * 100 + "%");
+              }
+            } else {
+              --oddeven;
+              if (counter < 38) {
+                if (counter != 37) {
+                  $(".anim-image").attr(
+                    "src",
+                    "./mainpage/" + ++counter + ".png"
+                  );
+                  $(".anim-image-2").fadeOut(function () {
+                    $(".anim-image").fadeIn();
+                  });
+
+                  switchCases(counter);
+
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                } else {
+                  $(".anim-image").attr(
+                    "src",
+                    "./mainpage/" + counter + ".png"
+                  );
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                }
               }
             }
           }
+        }, 300);
+      });
+    }
+  }, 30);
+} else {
+  var load = setInterval(function () {
+    $(".loading").text(loading + "%");
+    loading++;
+    let gray = "grayscale(" + (100 - loading) + "%)";
+
+    $(".logo-loading").css("filter", gray);
+
+    if (loading == 101) {
+      clearInterval(load);
+      $(".loading-logo").fadeOut();
+      $(".anim-image").attr("src", "./mainpage/1.png");
+      $(window).bind("mousewheel", function (event) {
+        if (timer) {
+          window.clearTimeout(timer);
         }
-      }, 100);
-    });
-  }
-}, 30);
+
+        timer = window.setTimeout(function () {
+          if (oddeven % 2 == 0) {
+            if (event.originalEvent.wheelDelta >= 0) {
+              ++oddeven;
+              if (counter > 1) {
+                $(".anim-image-2").attr(
+                  "src",
+                  "./mainpage/" + --counter + ".png"
+                );
+                $(".anim-image").fadeOut(function () {
+                  $(".anim-image-2").fadeIn();
+                });
+
+                $(".red").css("width", String(counter / 37) * 100 + "%");
+              }
+            } else {
+              --oddeven;
+              if (counter < 38) {
+                if (counter != 37) {
+                  $(".anim-image-2").attr(
+                    "src",
+                    "./mainpage/" + ++counter + ".png"
+                  );
+                  $(".anim-image").fadeOut(function () {
+                    $(".anim-image-2").fadeIn();
+                  });
+
+                  switchCases(counter);
+
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                } else {
+                  $(".anim-image-2").attr(
+                    "src",
+                    "./mainpage/" + counter + ".png"
+                  );
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                }
+              }
+            }
+          } else {
+            if (event.originalEvent.wheelDelta >= 0) {
+              ++oddeven;
+              if (counter > 1) {
+                $(".anim-image").attr(
+                  "src",
+                  "./mainpage/" + --counter + ".png"
+                );
+                $(".anim-image-2").fadeOut(function () {
+                  $(".anim-image").fadeIn();
+                });
+
+                switchCases(counter);
+                $(".red").css("width", String(counter / 37) * 100 + "%");
+              }
+            } else {
+              --oddeven;
+              if (counter < 38) {
+                if (counter != 37) {
+                  $(".anim-image").attr(
+                    "src",
+                    "./mainpage/" + ++counter + ".png"
+                  );
+                  $(".anim-image-2").fadeOut(function () {
+                    $(".anim-image").fadeIn();
+                  });
+
+                  switchCases(counter);
+
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                } else {
+                  $(".anim-image").attr(
+                    "src",
+                    "./mainpage/" + counter + ".png"
+                  );
+                  $(".red").css("width", String((counter / 37) * 100) + "%");
+                }
+              }
+            }
+          }
+        }, 300);
+      });
+    }
+  }, 30);
+}
 function switchCases(counter) {
-  console.log(counter);
   switch (counter) {
     case 7:
       $(".btn-vrclad").attr("href", "shtagoneba.html");
